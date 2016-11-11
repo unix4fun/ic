000f0385a0389a3c9cea616068b8ff103eaca2de
eau <eau-code@unix4fun.net>
2016-11-11 01:12:56 +0100

    starting tests on obfuscation first

---
+++ obf/obfuscate_test.go
@@ -0,0 +1,66 @@
+package obf
+
+import (
+	"testing"
+	"crypto/rand"
+)
+
+//  0x55        0x55
+// [0101 0101] [0101 0101]
+
+type iotest struct {
+	in      []byte // input
+	out     []byte // output
+	mask_lo byte
+	mask_hi byte
+}
+
+var tests = []iotest{
+	{[]byte("A"), nil, 0x55, 0x55},
+	{[]byte("A"), nil, 0x05, 0xf5},
+	{[]byte("F"), nil, 0xf5, 0x05},
+	{[]byte("F"), nil, 0xff, 0x00},
+}
+
+func TestObfuscateByte(t *testing.T) {
+	for i := 0; i < len(tests); i++ {
+		obfOut, err := ObfuscateByte(tests[i].in, tests[i].mask_hi, tests[i].mask_lo)
+		if err != nil {
+			t.Logf("ObfuscateByte error\n")
+			t.Fail()
+		}
+
+		outIn, err := DeobfuscateByte(obfOut, tests[i].mask_hi, tests[i].mask_lo)
+		if err != nil {
+			t.Logf("DeObfuscateByte error\n")
+			t.Fail()
+		}
+
+		if tests[i].in[0] != outIn[0] {
+			t.Logf("matching error %02x vs %02x\n", tests[i].in, outIn[0])
+			t.Fail()
+		}
+	}
+
+	for i := 0 ; i < 100000 ; i++ {
+		obfIn := make([]byte, 1)
+		rand.Read(obfIn)
+
+		obfOut, err := ObfuscateByte(obfIn, 0x55, 0x55)
+		if err != nil {
+			t.Logf("ObfuscateByte error\n")
+			t.Fail()
+		}
+
+		outIn, err := DeobfuscateByte(obfOut, 0x55, 0x55)
+		if err != nil {
+			t.Logf("DeObfuscateByte error\n")
+			t.Fail()
+		}
+
+		if obfIn[0] != outIn[0] {
+			t.Logf("matching error %02x vs %02x\n", obfIn[0], outIn[0])
+			t.Fail()
+		}
+	}
+}
